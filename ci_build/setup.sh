5a8f0b8aa78d4fed3c1cbd89e92744e57d1f15be
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2021-12-27 13:34:42 -0800

    Use  install llvm

--- ci_build/setup.sh
+++ ci_build/setup.sh
@@ -3,5 +3,8 @@ set -e
 
 yum -y update
 yum -y install python3-pip
-yum -y install rustc
+yum install llvm7.0
+//reconfigure and recompile surticata
+./configure --enable-rust
+make
 pip3 install virtualenv
