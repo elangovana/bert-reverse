2566a03254d3d6d5e57a3349899228338dd0e905
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2021-12-27 14:47:21 -0800

    Update setup use apt-get

--- ci_build/setup.sh
+++ ci_build/setup.sh
@@ -1,8 +1,8 @@
 # exit when any command fails
 set -e
 
-yum -y update
-yum -y install python3-pip
-yum -y install llvm7.0
-yum -y install rustc
+apt-get -y update
+apt-get -y install python3-pip
+#apt-get -y install llvm7.0
+apt-get -y install rustc
 pip3 install virtualenv
