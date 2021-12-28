bcb44619b495f036efd788e8618e1f8619f69418
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2021-12-27 17:51:51 -0800

    Remove unused files

--- ci_build/setup.sh
+++ ci_build/setup.sh
@@ -1,8 +1,4 @@
 # exit when any command fails
 set -e
 
-apt-get -y update
-apt-get -y install python3-pip
-#apt-get -y install llvm7.0
-apt-get -y install rustc
-pip3 install virtualenv
+echo no pre-requisites to install
