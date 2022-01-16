34f5196130c661f69e2b1b1fdbb6ebea1bfd5726
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2022-01-16 10:56:53 -0800

    Add batch predictions

---
+++ src/inference/batch_predict.py
@@ -0,0 +1,156 @@
+import glob
+import json
+import logging
+import os
+import traceback
+from pathlib import Path
+from typing import Dict
+
+from dataset_builder import DatasetBuilder
+from inference.predictor import Predictor
+from locator import Locator
+
+
+class BatchPredict:
+
+    @property
+    def _logger(self):
+        return logging.getLogger(__name__)
+
+    def predict_from_directory(self, datajson, base_artefacts_dir, is_ensemble, output_dir=None, numworkers=None,
+                               batch=32, additional_args=None, raw_data_reader_func=None, filter_func=None):
+        data_files = [datajson]
+        if os.path.isdir(datajson):
+            data_files = glob.glob("{}/*.jsonl".format(datajson))
+
+        for d in data_files:
+            output_file = "{}.json".format(os.path.join(output_dir, Path(d).name)) if output_dir else None
+            self._logger.info("Running inference on file {} with output in {}".format(d, output_file))
+            try:
+                prediction = self.predict_from_file(d, base_artefacts_dir, is_ensemble, output_file, numworkers, batch,
+                                                    additional_args, raw_data_reader_func, filter_func)
+
+                yield prediction
+            except AssertionError as ex:
+                self._logger.warning("Failed processing file {} due to error {}".format(d, traceback.format_exc()))
+                yield None
+
+    def predict_from_file(self, data_file, base_artifacts_dir, is_ensemble, output_file=None, numworkers=None, batch=32,
+                          additional_args=None, raw_data_reader_func=None, filter_func=None):
+        additional_args = additional_args or {}
+
+        self._logger.info(f"Processing data file {data_file}")
+
+        artifacts_directories = []
+        if is_ensemble:
+            for d in os.listdir(base_artifacts_dir):
+                artifacts_dir = os.path.join(base_artifacts_dir, d)
+                artifacts_directories.append(artifacts_dir)
+        else:
+            artifacts_directories = [base_artifacts_dir]
+
+        # Load params
+        output_config = os.path.join(artifacts_directories[0], "training_config_parameters.json")
+        with open(output_config, "r") as f:
+            train_args = json.load(f)
+
+        train_args = {**train_args, **additional_args}
+
+        self._logger.info("Using args :{}".format(train_args))
+
+        # Dataset Builder
+        model_factory_name = train_args["modelfactory"]
+        dataset_builder = DatasetBuilder(val_data=data_file, dataset_factory_name=train_args["datasetfactory"],
+                                         tokenisor_factory_name=model_factory_name,
+                                         num_workers=numworkers, batch_size=batch,
+                                         addition_args_dict=train_args)
+        # Load ensemble
+        models = []
+        for artifact_dir in artifacts_directories:
+            # Persist params
+            output_config = os.path.join(artifacts_directories[0], "training_config_parameters.json")
+            with open(output_config, "r") as f:
+                train_args = json.load(f)
+
+            model_factory = Locator().get(model_factory_name)
+            model = model_factory.get_model(dataset_builder.num_classes, checkpoint_dir=artifact_dir, **train_args)
+
+            models.append(model)
+
+        models = models[0]
+        predictions_data = Predictor().predict(models,
+                                               dataset_builder.get_val_dataloader())
+
+        raw_data_iter = raw_data_reader_func(data_file) if raw_data_reader_func else None
+        self.write_results_to_file(predictions_data, dataset_builder.get_label_mapper(),
+                                   output_file,
+                                   raw_data_iter,
+                                   filter_func)
+
+        self._logger.info(f"Completed file {data_file}")
+
+        return predictions_data
+
+    def write_results_to_file(self, predictions_data_tuple, label_mapper,
+                              output_file,
+                              raw_data_iter=None, filter_func=None):
+
+        result = []
+
+        predictions_tensor = predictions_data_tuple[0]
+        confidence_scores = predictions_data_tuple[1]
+        print(predictions_data_tuple[0].shape)
+        # If no filter pass everything through
+        default_filter = lambda p, c: True
+        filter_func = filter_func or default_filter
+
+        if raw_data_iter is None:
+            raw_data_iter = [None] * len(predictions_tensor)
+
+        assert len(raw_data_iter) == len(
+            predictions_tensor), "The length of raw data iterator {} doesnt match the prediction len {}".format(
+            len(raw_data_iter), len(predictions_tensor))
+
+        # Convert indices to labels
+        for i, raw_data in enumerate(raw_data_iter):
+
+            pred_i_tensor = predictions_tensor[i]
+            conf_i_tensor = confidence_scores[i]
+
+            pred_i = pred_i_tensor.cpu().tolist()
+            conf_i = conf_i_tensor.cpu().tolist()
+
+            label_mapped_prediction = [label_mapper.reverse_map(pi) for pi in pred_i]
+            predicted_raw_text = ""
+            for t in label_mapped_prediction:
+                if not t.startswith("##"):
+                    predicted_raw_text = predicted_raw_text + " "
+                predicted_raw_text = predicted_raw_text + t
+
+            predicted_confidence = conf_i
+
+            if not filter_func(label_mapped_prediction, predicted_confidence): continue
+
+            r = {
+                "prediction": label_mapped_prediction,
+                "confidence": predicted_confidence,
+                "predicted_raw_text": predicted_raw_text
+            }
+
+            r = {**r}
+
+            # Add raw data if available
+            if isinstance(raw_data, Dict):
+                r = {**raw_data, **r}
+            else:
+                r["raw_data"] = raw_data
+
+            result.append(r)
+
+        self._logger.info("Records to write: {}".format(len(result)))
+
+        if len(result) > 0:
+            self._logger.info(f"Writing to file {output_file}")
+            # Write json to file
+            with open(output_file, "w") as f:
+                json.dump(result, f)
---
+++ src/inference/reverse_lang_mnli_batch_predict.py
@@ -0,0 +1,94 @@
+import argparse
+import json
+import logging
+import sys
+
+from inference.batch_predict import BatchPredict
+
+
+class ReverseLangMnliBatchPredict:
+
+    def __init__(self, batch_predict, use_filter=False):
+        self.use_filter = use_filter
+        self.batch_predict = batch_predict
+
+    @property
+    def _logger(self):
+        return logging.getLogger(__name__)
+
+    def _get_mnli_inference_reader(self, data_File):
+        lines = []
+        with open(data_File) as f:
+            for l in f:
+                data_l = json.loads(l)
+                lines.append(data_l["sentence1"])
+                lines.append(data_l["sentence2"])
+        return lines
+
+    def predict_from_directory(self, datajson, base_artefacts_dir, is_ensemble, output_dir, numworkers=None, batch=32,
+                               additional_args=None, raw_data_reader_func=None, filter_func=None):
+        raw_data_reader_func = raw_data_reader_func or self._get_mnli_inference_reader
+
+        # Use filter if
+        default_filter = lambda p, c: True
+        if self.use_filter:
+            self._logger.info("Using filter ")
+            default_filter = lambda p, c: True
+        filter_func = filter_func or default_filter
+
+        # Invoke underlying batch predict
+        return list(self.batch_predict.predict_from_directory(datajson, base_artefacts_dir,
+                                                              is_ensemble, output_dir=output_dir,
+                                                              numworkers=numworkers, batch=batch,
+                                                              additional_args=additional_args,
+                                                              raw_data_reader_func=raw_data_reader_func,
+                                                              filter_func=filter_func))
+
+    def predict_from_file(self, datajson, base_artefacts_dir, is_ensemble, output_file, numworkers=None, batch=32,
+                          additional_args=None, raw_data_reader_func=None, filter_func=None):
+        raw_data_reader_func = raw_data_reader_func or self._get_mnli_inference_reader
+        return self.batch_predict.predict_from_file(datajson, base_artefacts_dir,
+                                                    is_ensemble, output_file=output_file,
+                                                    numworkers=numworkers, batch=batch,
+                                                    additional_args=additional_args,
+                                                    raw_data_reader_func=raw_data_reader_func,
+                                                    filter_func=filter_func
+                                                    )
+
+
+def parse_args_run():
+    global args, additional_dict
+    parser = argparse.ArgumentParser()
+    parser.add_argument("datajson",
+                        help="The json data to predict")
+    parser.add_argument("artefactsdir", help="The base of artefacts dir that contains directories of model, vocab etc")
+    parser.add_argument("outdir", help="The output dir")
+    parser.add_argument("--log-level", help="Log level", default="INFO", choices={"INFO", "WARN", "DEBUG", "ERROR"})
+    parser.add_argument("--numworkers", help="The number of workers to use", type=int, default=None)
+    parser.add_argument("--batch", help="The batchsize", type=int, default=32)
+    parser.add_argument("--ensemble", help="Set to 1 if ensemble model", type=int, default=0, choices={0, 1})
+    parser.add_argument("--filter", help="Set to 1 if ensemble model", type=int, default=0, choices={0, 1})
+
+    args, additional_args = parser.parse_known_args()
+    print(args.__dict__)
+    # Convert additional args into dict
+    additional_dict = {}
+    for i in range(0, len(additional_args), 2):
+        additional_dict[additional_args[i].lstrip("--")] = additional_args[i + 1]
+    print(additional_dict)
+
+    # Set up logging
+    logging.basicConfig(level=logging.getLevelName(args.log_level), handlers=[logging.StreamHandler(sys.stdout)],
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    ReverseLangMnliBatchPredict(BatchPredict(), use_filter=args.filter).predict_from_directory(args.datajson,
+                                                                                               args.artefactsdir,
+                                                                                               args.ensemble,
+                                                                                               args.outdir,
+                                                                                               args.numworkers,
+                                                                                               args.batch,
+                                                                                               additional_dict)
+
+
+if "__main__" == __name__:
+    parse_args_run()
